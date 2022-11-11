e2ebc5ce832b61e9abade47816477dc7604e7e8a
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-11-11 00:01:22 -0600

    Add valid input checker

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const results = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const errorMessage = document.getElementById("errorMessage");
 
-new URLSearchParams(window.location.search).forEach((value, name) => {
-    results.append(`${name}: ${value}, `)
-});
+function checkForValidInput(e) {
+    errorMessage.innerText = "";
+    if (password.value !== confirmPassword.value) {
+        errorMessage.append("Passwords must be the same");
+        errorMessage.append(document.createElement("br"));
+        e.preventDefault();
+    }
+    if (password.value.length < 8) {
+        errorMessage.append("Password must be at least 8 characters");
+        e.preventDefault();
+    }
+}
+
+submitButton.addEventListener("click", checkForValidInput);
